23a2a283dbdf51607366222f2e5737ee1a1677fe
Sawyerf <sawyer.flink@protonmail.ch>
2019-01-31 04:27:00 +0100

    Fix: uid & secret

--- src/main.py
+++ src/main.py
@@ -4,11 +4,14 @@ from location   import Location
 from slack      import Slack
 
 slack_token = os.environ["SLACK_API_TOKEN"]
+ft_uid = os.environ["ft_uid"]
+ft_secret = os.environ["ft_secret"]
+
 slack = Slack(slack_token)
 lct = Location(
         slack=slack,
-        uid="988bf8f381824571cfa3b4dc1e591904ccdf00549a2f86b7d96d2c380783b4bc",
-        secret="27da4f23ef3e682f6a3e7f8ba3db55e572582e038ae65b349bd3f4780e9810ec")
+        uid=ft_uid,
+        secret=ft_secret)
 
 print(lct.token)
 slack.run(lct)
