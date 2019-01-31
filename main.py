a15a2957456bf0fa6f87abec3f3cf19de6b93be5
sawyerf <sawyer.flink@protonmail.ch>
2019-01-31 17:27:17 +0000

    Add: location

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import os
 from slackclient import SlackClient
+import requests
+
+def get_token():
+    uid = "988bf8f381824571cfa3b4dc1e591904ccdf00549a2f86b7d96d2c380783b4bc"
+    secret = "27da4f23ef3e682f6a3e7f8ba3db55e572582e038ae65b349bd3f4780e9810ec"
+    d = {'grant_type': 'client_credentials',
+        'client_id': uid,
+        'client_secret': secret}
+    r = requests.post("https://api.intra.42.fr/oauth/token", data=d)
+    return r.json()['access_token']
+
+def location_of(user, token):
+    lol = requests.get("https://api.intra.42.fr/v2/users/apeyret", headers={'Authorization': 'Bearer ' + token})
+    dat = lol.json()['location']
 
 slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 
+
+token = get_token()
 print(sc.api_call(
   "chat.postMessage",
-  channel="CFTUZTEM7",
-  text="Hello from Python! :tada:",
+  channel="CCEELHRR6",
+  text="""apeyret: {}
+  oel-ayad: {}
+  glavigno: {}
+  cvignal: {}
+  thdervil: {}""".format(location_of("apeyret", token), location_of("oel-ayad", token), location_of("glavigno", token), location_of("cvignal", token), location_of("thdervil", token)),
 ))
 print("lol\n")
