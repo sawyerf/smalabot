1b39b42d19e9f7ac3c07105a559892101ed08c3e
sawyerf <sawyer.flink@protonmail.ch>
2019-01-31 18:54:51 +0000

    Add: \!lct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from slackclient import SlackClient
 import requests
 
@@ -11,29 +12,47 @@ def get_token():
     r = requests.post("https://api.intra.42.fr/oauth/token", data=d)
     return r.json()['access_token']
 
-def location_of(user, token):
+def location_of(token, user):
     lol = requests.get("https://api.intra.42.fr/v2/users/" + user, headers={'Authorization': 'Bearer ' + token})
     dat = lol.json()['location']
     if dat == None:
-        print("Unavailable")
-        return "Unavailable"
-    print(dat)
-    return dat
+        return ''
+    return user + ': `' + dat + '`\n'
 
+def location_all(token):
+    smala = ['apeyret', 'oel-ayad', 'cvignal', 'clfoltra', 'glavigno', 'sboulaao', 'thdervil', 'gchainet']
+    lct = ''
+    for user in smala:
+        lct += location_of(token, user)
+    return lct
+
+def send_location(token, users):
+    lct = ''
+    if len(users) == 0:
+        lct = location_all(token)
+    else:
+        for user in users:
+            tmp = location_of(token, user)
+            if tmp != '':
+                lct += tmp;
+            else:
+                lct += user + ': `Unavailable`\n'
+    sc.api_call(
+        "chat.postMessage",
+        channel="CFTUZTEM7",
+        text=lct)
+
+def router(msg, token):
+    if msg[:4] == '!lct':
+        send_location(token, msg.split(" ")[1:])
 
 slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
-
-
 token = get_token()
-print(token)
-sc.api_call(
-    "chat.postMessage",
-    channel="CFTUZTEM7",
-    text="""apeyret: {}
-oel-ayad: {}
-glavigno: {}
-cvignal: {}
-thdervil: {}
-clfoltra: {}""".format(location_of("apeyret", token), location_of("oel-ayad", token), location_of("glavigno", token), location_of("cvignal", token), location_of("thdervil", token), location_of("clfoltra", token)),
-)
+if sc.rtm_connect():
+  while sc.server.connected is True:
+      lol = sc.rtm_read()
+      for msg in lol:
+          if msg['type'] == 'message':
+              if msg['channel'] == 'CFTUZTEM7':
+                    router(msg['text'], token)
